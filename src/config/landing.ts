e94068a9723569aa0e5f3c28329e1d1251501760
The Wizard of Hahz <info@luvnft.com>
2024-05-29 21:23:57 -0400

    updated

--- src/config/landing.ts
+++ src/config/landing.ts
@@ -106,31 +106,121 @@ export const testimonials = [
       "The free ATL5D channel for general Atlanta talk.",
   },
   {
-    name: "David Johnson",
-    job: "DevOps Engineer",
-    image: "https://randomuser.me/api/portraits/men/3.jpg",
-    review:
-      "Thanks to next-saas-stripe-starter, I was able to streamline the entire process and get payments up and running in no time.",
+    name: "#INTRODUCE-YA-SELF",
+    job: "🆓",
+    image: "https://em-content.zobj.net/source/apple/391/microphone_1f3a4.png",
+    review: `Grab the mic and let ATL5D know who you are, what you do, what zone you are from, and your <a href="https://phantom.app" target="_blank" rel="noopener noreferrer">Phantom.app</a> Ethereum address to receive $ATL.`,
   },
   {
-    name: "Michael Wilson",
-    job: "Project Manager",
-    image: "https://randomuser.me/api/portraits/men/5.jpg",
+    name: "#BARTER-SELL",
+    job: "▶️",
+    image: "https://em-content.zobj.net/source/apple/391/handshake_1f91d.png",
     review:
-      "I'm impressed by the quality of code and clear documentation of next-saas-stripe-starter. Kudos to the team!",
+      "Purchase D-commerce at a low price from the ATL5D plug-in on the Discord server's shop tab as a product, then flip it by selling it to your tribe to unlock your PPV channel, services, or products.",
   },
   {
-    name: "Sophia Garcia",
-    job: "Data Analyst",
-    image: "https://randomuser.me/api/portraits/women/6.jpg",
+    name: "#NEIGHBORHOODS",
+    job: "▶️",
+    image: "https://em-content.zobj.net/source/apple/391/house_1f3e0.png",
     review:
-      "next-saas-stripe-starter provided me with the tools I needed to efficiently manage user data. Thank you so much!",
+      "Discover what's happening in real-time in every neighborhood in Atlanta in one channel. Subscribe to the Gold Room tier to create a blockchain HOA. Send Bitcoin L$VE to your neighbor. Buy or sell your What3Words blocks in your neighborhood.",
   },
   {
-    name: "Emily Brown",
-    job: "Marketing Manager",
-    image: "https://randomuser.me/api/portraits/women/4.jpg",
+    name: "#ASK-ATL",
+    job: "▶️",
+    image: "https://em-content.zobj.net/source/apple/391/heart-exclamation_2763-fe0f.png",
     review:
-      "next-saas-stripe-starter has been an invaluable asset in my role as a marketing manager. With its seamless integration with Stripe, I've been able to launch targeted marketing campaigns with built-in payment functionality, allowing us to monetize our products and services more effectively.",
+      "If you want to know something in Atlanta then tap into the #ask-atl channel and create a poll with the command of /poll.",
+  },
+  {
+    name: "#ONLY-PETS",
+    job: "▶️",
+    image: "https://em-content.zobj.net/source/apple/391/smiling-cat-with-heart-eyes_1f63b.png",
+    review:
+      "Buy,sell or discuss pets in the #only-pets channel.",
+  },
+  {
+    name: "#TRANSPORTATION",
+    job: "▶️",
+    image: "https://em-content.zobj.net/source/apple/391/vertical-traffic-light_1f6a6.png",
+    review:
+      "Carpool, find a local driver or someone to deliver your products locally and pay them in $ATL money live as event episode.",
+  },
+  {
+    name: "#GM",
+    job: "▶️",
+    image: "https://em-content.zobj.net/source/apple/391/sun-with-face_1f31e.png",
+    review:
+      "Start your day off with positive vibes in the GM channel.",
+  },
+  {
+    name: "#RNTALS",
+    job: "▶️",
+    image: "https://em-content.zobj.net/source/apple/391/houses_1f3d8-fe0f.png",
+    review:
+      "Post a reel of your rental on social media then embed the video in the #rntals channel with the command /convert. Communicate in one channel and accept Bitcoin $RNT with no middleman fees to create affordable Atlanta housing.",
+  },
+  {
+    name: "#HAIR-INDUSTRY",
+    job: "▶️",
+    image: "https://em-content.zobj.net/source/apple/391/large-red-circle_1f534.png",
+    review:
+      "Atlanta is the hair capital, so it was only right that stylists have their own channel for promoting d-commerce to unlock their services, bookings, and to accept Bitcoin HAI$ money.",
+  },
+  {
+    name: "#FOODIES",
+    job: "▶️",
+    image: "https://em-content.zobj.net/source/apple/391/hamburger_1f354.png",
+    review:
+      "Promote the local Atlanta food industry to earn $ATL money or D-commerce merch to unlock food IRL.",
+  },
+  {
+    name: "#MUSIC-INDUSTRY",
+    job: "▶️",
+    image: "https://em-content.zobj.net/source/apple/391/musical-note_1f3b5.png",
+    review:
+      "Subscribe, then tokenize your music to sell it in the music channel to your fans, who can resell it for more than they paid. Earn royalties on all secondary sales, and we'll promote it. Earn $1M $ATL for creating the #1 voted $ATL Booty memecoin song.",
+  },
+  {
+    name: "#GAMERS",
+    job: "▶️",
+    image: "https://em-content.zobj.net/source/apple/391/video-game_1f3ae.png",
+    review:
+      "ATL5D united Atlanta gamers in one channel, monetize your content with a branded Discord and ATLverse channel. Promote live gaming events in the ATLverse.",
+  },
+  {
+    name: "#VOICE-CHAT",
+    job: "▶️",
+    image: "https://em-content.zobj.net/source/apple/391/couch-and-lamp_1f6cb-fe0f.png",
+    review:
+      "Listen to live ATL5D entertainment from your couch.",
+  },
+  {
+    name: "#DJ-BOOTH",
+    job: "▶️",
+    image: "https://em-content.zobj.net/source/apple/391/headphone_1f3a7.png",
+    review:
+      "Listen to your favorite Atlanta DJ from anywhere in the world. Show them love by making it rain $ATL money on em.",
+  },
+  {
+    name: "#COOKING",
+    job: "▶️",
+    image: "https://em-content.zobj.net/source/apple/391/woman-cook-medium-skin-tone_1f469-1f3fd-200d-1f373.png",
+    review:
+      "Find out who is cooking up a fresh ATL5D plate that you can buy as D-commerce. Sell recipes as D-commerce. Create affordable food.",
+  },
+  {
+    name: "#CAREER",
+    job: "▶️",
+    image: "https://em-content.zobj.net/source/apple/391/movie-camera_1f3a5.png",
+    review:
+      "ATL5D is the new Hollywood. Buy and sell movies, short films, or commercial ads that we promote on our socials. The new stars are being born in our PPV channels. Throw a ticket-gated live streamed movie event to a crowd of 500 attendees in the ATLverse.",
+  },
+  {
+    name: "#MOVIE-INDUSTRY",
+    job: "▶️",
+    image: "https://em-content.zobj.net/source/apple/391/movie-camera_1f3a5.png",
+    review:
+      "ATL5D is the new Hollywood. Buy and sell movies, short films, or commercial ads that we promote on our socials. The new stars are being born in our PPV channels. Throw a ticket-gated live streamed movie event to a crowd of 500 attendees in the ATLverse.",
   },
 ];
