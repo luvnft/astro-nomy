dbdf61a72d78ccb8ff2d3ad6b15e9bc0965b774c
The Wizard of Hahz <info@luvnft.com>
2024-06-24 06:50:25 -0400

    updated

--- src/config/landing.ts
+++ src/config/landing.ts
@@ -96,36 +96,36 @@ export const testimonials = [
     job: "🆓",
     image: "https://em-content.zobj.net/source/apple/391/bottle-with-popping-cork_1f37e.png",
     review:
-      "All ATL5D live episodes are scheduled as events that viewers can register for and receive $ATL money for attending.",
+      "ATL5D Discord has DIY event scheduling for live ATL5D.shop episodes, Market.atl5d.com drops, bookings or ATLverse events.",
   },
   {
     name: "#GENERAL-CHAT",
     job: "🆓",
     image: "https://em-content.zobj.net/source/apple/391/thought-balloon_1f4ad.png",
     review:
-      "The free ATL5D channel for general Atlanta talk.",
+      "See what's on Atlanta's mind",
     link: "https://discord.gg/x3jKSusHk5",
   },
   {
     name: "#INTRO",
     job: "🆓",
     image: "https://em-content.zobj.net/source/apple/391/microphone_1f3a4.png",
-    review: `Grab the mic and let ATL5D know who you are, what you do, what zone you are from, and your ATL5D Coinbase wallet address to receive $ATL tips.`,
+    review: `Grab the mic and let ATL5D know who you are, what you do, and your ATL5D Wallo address to receive $ATL tips.`,
     link: "https://discord.gg/93YCshjGHb",
   },
   {
-    name: "#BARTER-SELL",
-    job: "▶️",
+    name: "#MARKET",
+    job: "🆓",
     image: "https://em-content.zobj.net/source/apple/391/handshake_1f91d.png",
     review:
-      "Purchase D-commerce at a low price from the ATL5D plug-in on the Discord server's shop tab as a product, then flip it by selling it to your tribe to unlock your PPV channel, services, or products.",
+      "Discover new ATL D-merch is for sale that can be resold for more than you purchased it for.",
   },
   {
     name: "#NEIGHBORHOODS",
     job: "▶️",
     image: "https://em-content.zobj.net/source/apple/391/house_1f3e0.png",
     review:
-      "Discover what's happening in real-time in every neighborhood in Atlanta in one channel. Subscribe to the Gold Room tier to create a blockchain HOA. Send Bitcoin L$VE to your neighbor. Buy or sell your What3Words blocks in your neighborhood.",
+      "Discover what's happening in real-time in every neighborhood in Atlanta in one channel.",
   },
   {
     name: "#ASK-ATL",
@@ -142,11 +142,11 @@ export const testimonials = [
       "Buy,sell or discuss pets in the #only-pets channel.",
   },
   {
-    name: "#TRANSPORTATION",
+    name: "#CABCAST",
     job: "▶️",
     image: "https://em-content.zobj.net/source/apple/391/vertical-traffic-light_1f6a6.png",
     review:
-      "Carpool, find a local driver or someone to deliver your products locally and pay them in $ATL money live as event episode.",
+      "Cabcast is the new Uber with no middleman, drivers earn 💯%. Challenge a driver with a live delivery event episode.",
   },
   {
     name: "#GM",
@@ -160,35 +160,42 @@ export const testimonials = [
     job: "▶️",
     image: "https://em-content.zobj.net/source/apple/391/houses_1f3d8-fe0f.png",
     review:
-      "Post a reel of your rental on social media then embed the video in the #rntals channel with the command /convert. Communicate in one channel and accept Bitcoin $RNT with no middleman fees to create affordable Atlanta housing.",
+      "RNTALS is the new Airbnb. Accept $RNT for your properties with no middleman fees. ",
   },
   {
     name: "#HAIR",
     job: "▶️",
     image: "https://em-content.zobj.net/source/apple/391/large-red-circle_1f534.png",
     review:
-      "Atlanta hair stylist sell D-commerce Hair to unlock their services, bookings, and to accept Bitcoin HAI$ money.",
+      "Atlanta hair stylist sells or teaches hair, bookings, and accepts Bitcoin HAI$ money with middleman fees.",
   },
   {
     name: "#FOODIE",
     job: "▶️",
     image: "https://em-content.zobj.net/source/apple/391/hamburger_1f354.png",
     review:
-      "Promote the local Atlanta food industry to earn $ATL money or D-commerce merch to unlock food IRL.",
+      "Promote the local Atlanta food industry to earn $ATL rewards, food and money.",
   },
   {
     name: "#MUSIC",
     job: "▶️",
     image: "https://em-content.zobj.net/source/apple/391/musical-note_1f3b5.png",
     review:
-      "Buy/sell D-commerce audio in the music channel to your fans, who can resell it for more than they paid. Earn royalties on all secondary sales, and we'll promote it. Earn $1M $ATL for creating the #1 voted $ATL Booty memecoin song.",
+      "ATL5D is the music industry. Head over to ATL5D.shop to tokenize your music to earn 💯%.",
+  },
+  {
+    name: "#SPIRITUAL",
+    job: "▶️",
+    image: "https://em-content.zobj.net/source/apple/391/sparkles_2728.png",
+    review:
+      "Discover your new ATL5D spiritual tribe.",
   },
   {
     name: "#GAMERS",
     job: "▶️",
     image: "https://em-content.zobj.net/source/apple/391/video-game_1f3ae.png",
     review:
-      "ATL5D gamers united in one #gamers channel, play ATL5D games or ATLverse games. Casino gamers wanted.",
+      "ATL5D gamers united in one #gamers channel, play ATL5D games, ATLverse games or casino games.",
   },
   {
     name: "#VOICE-CHAT",
@@ -202,41 +209,41 @@ export const testimonials = [
     job: "▶️",
     image: "https://em-content.zobj.net/source/apple/391/headphone_1f3a7.png",
     review:
-      "Listen to your favorite Atlanta DJ from anywhere in the world. Show them love by making it rain $ATL money on em.",
+      "Listen to your favorite Atlanta DJ from anywhere in the world. Show them love by making it rain $ATL on em.",
   },
   {
     name: "#COOKING",
     job: "▶️",
     image: "https://em-content.zobj.net/source/apple/391/woman-cook-medium-skin-tone_1f469-1f3fd-200d-1f373.png",
     review:
-      "Find out who is cooking up a fresh ATL5D plate that you can buy as D-commerce. Sell recipes as D-commerce. Create affordable food.",
+      "Find out who is cooking up a fresh ATL plate that you can buy in the ATL5D.shop to create affordable food options.",
   },
   {
     name: "#CAREER",
     job: "▶️",
     image: "https://em-content.zobj.net/source/apple/391/briefcase_1f4bc.png",
     review:
-      "Network with ATLiens in the came career as you, talk about the job economy. Post jobs that pay in $ATL money or Bitcoin L$VE.",
+      "See who's hiring in ATL.",
   },
   {
     name: "#MOVIE",
     job: "▶️",
     image: "https://em-content.zobj.net/source/apple/391/movie-camera_1f3a5.png",
     review:
-      "ATL5D is the new Hollywood. Buy and sell movies, short films, or commercial ads that we promote on our socials. The new stars are being born in our PPV channels. Throw a ticket-gated live streamed movie event to a crowd of 500 attendees in the ATLverse.",
+      "ATL5D is the new Hollywood. Buy and sell movies, short films, or commercial ads that we promote on our socials.",
   },
   {
     name: "#PICS",
     job: "▶️",
     image: "https://em-content.zobj.net/source/apple/391/camera-with-flash_1f4f8.png",
     review:
-      "Claim your FREE ATLnight🔴 pic as D-commerce or buy and sell pics as D-commerce.",
+      "Claim your FREE ATLnight🔴 pic that is airdropped to your Wallo so you can sell it or pass it down your kids as a digital asset.",
   },
   {
     name: "#BEAUTY",
     job: "▶️",
     image: "https://em-content.zobj.net/source/apple/391/nail-polish_1f485.png",
     review:
-      "Join the Beauty channel to find local beauticians selling local D-commerce services or products.",
+      "Join the Beauty channel to find local beauticians ATL5D.shop merch.",
   },
 ];
