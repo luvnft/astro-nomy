14656b3a3f2707f826cafca80171b6fc80c1e8ad
The Wizard of Hahz <info@luvnft.com>
2024-07-15 21:32:19 -0400

    updated

--- src/config/landing.ts
+++ src/config/landing.ts
@@ -3,8 +3,7 @@ import type { InfoLdg } from '@/types';
 export const infos: InfoLdg[] = [
   {
     title: 'ATL5D.SHOP',
-    description:
-      'Sell merch to Atlanta live with 0% commission fees, with an ATL Discord CRM, ATL Podcast, ATL leader board, ATL meme marketing, and a simple public $ATL loyalty rewards system with an ATL Wallet trust account. [Shop ATL5D](https://atl5d.com)',
+    description: `Sell merch to Atlanta live with 0% commission fees, with an ATL Discord CRM, ATL Podcast, ATL leader board, ATL meme marketing, and a simple public $ATL loyalty rewards system with an ATL Wallet trust account. <a href="https://atl5d.com">Shop ATL5D</a>`,
     image: '/images/blog/placeholder-about.jpg',
     list: [
       {
@@ -13,39 +12,35 @@ export const infos: InfoLdg[] = [
         icon: 'lucide:shopping-bag',
       },
       {
-        title: 'Meme Marketing',
-        description: 'Market your business with branded CapCut memes and branded effects.',
+        title: 'Book an ATLfluencer',
+        description: 'Book an ATLfluencer for one (remote) or two hours (on-site) to host a live flash sale',
         icon: 'lucide:megaphone',
       },
       {
-        title: '3 Word Addy',
-        description:
-          'Ship to a What3Words.com ///keep.it.simple or find a CabCast driver in the ATL5D Discord to ship it live.',
+        title: 'Pull up',
+        description: 'Be the first to pull up to live flash sale near you on the What3Words.com ///keep.it.simple map',
         icon: 'lucide:map-pin',
       },
     ],
   },
   {
     title: "Seamless Integration",
-    description:
-      "Integrate our open-source SaaS seamlessly into your existing workflows. Effortlessly connect with your favorite tools and services for a streamlined experience.",
+    description: "Integrate our open-source SaaS seamlessly into your existing workflows. Effortlessly connect with your favorite tools and services for a streamlined experience.",
     image: "/images/blog/placeholder-about.jpg",
     list: [
       {
         title: "Flexible",
-        description:
-          "Customize your integrations to fit your unique requirements.",
+        description: "Customize your integrations to fit your unique requirements.",
         icon: "lucide:laptop",
       },
       {
         title: "Efficient",
-        description: "Streamline your processes and reducing manual effort.",
+        description: "Streamline your processes and reduce manual effort.",
         icon: "lucide:search",
       },
       {
         title: "Reliable",
-        description:
-          "Rely on our robust infrastructure and comprehensive documentation.",
+        description: "Rely on our robust infrastructure and comprehensive documentation.",
         icon: "lucide:settings",
       },
     ],
@@ -55,14 +50,12 @@ export const infos: InfoLdg[] = [
 export const features = [
   {
     title: "The Atlanta Housing Crisis Solution",
-    description:
-      "Welcome to ATL5D: Learn how the blockchain economy works for everyone in Atlanta",
+    description: "Welcome to ATL5D: Learn how the blockchain economy works for everyone in Atlanta",
     link: "https://atl5d.arvrtise.com/blog/welcome-to-atl5d/",
   },
   {
     title: "Feature 2",
-    description:
-      "Learn how Bitcoin $RNT creates affordable Atlanta housing and solves rental scams",
+    description: "Learn how Bitcoin $RNT creates affordable Atlanta housing and solves rental scams",
     link: "https://atl5d.arvrtise.com/blog/how-bitcoin-rnt-solves-atlanta-housing-crisis/",
   },
   {
@@ -72,20 +65,17 @@ export const features = [
   },
   {
     title: "Tokenizing Atlanta",
-    description:
-      "Tokenizing Atlanta Real-World Assets: Building a Local Blockchain Economy through D-commerce ",
+    description: "Tokenizing Atlanta Real-World Assets: Building a Local Blockchain Economy through D-commerce",
     link: "https://atl5d.arvrtise.com/blog/tokenizing-atlanta-rwa/",
   },
   {
     title: "Feature 5",
-    description:
-      "Subscribe to ATL5D Discord to claim your FREE $ATL B🍑🍑ty memecoin money ",
+    description: "Subscribe to ATL5D Discord to claim your FREE $ATL B🍑🍑ty memecoin money",
     link: "https://discord.com/servers/atl5d-1244450286337003520",
   },
   {
     title: "Feature 6",
-    description:
-      "Join our FREE ATL5D Discord until you subscribe to unlock local PPV channels",
+    description: "Join our FREE ATL5D Discord until you subscribe to unlock local PPV channels",
     link: "https://discord.gg/QQeFcekQyx",
   },
 ];
@@ -95,155 +85,134 @@ export const testimonials = [
     name: "#EVENTS",
     job: "🆓",
     image: "https://em-content.zobj.net/source/apple/391/bottle-with-popping-cork_1f37e.png",
-    review:
-      "ATL5D Discord has DIY event scheduling for live ATL5D.shop episodes, Market.atl5d.com drops, bookings or ATLverse events.",
+    review: "ATL5D Discord has DIY event scheduling for live ATL5D.shop episodes, Market.atl5d.com drops, bookings or ATLverse events.",
   },
   {
     name: "#GENERAL-CHAT",
     job: "🆓",
     image: "https://em-content.zobj.net/source/apple/391/thought-balloon_1f4ad.png",
-    review:
-      "See what's on Atlanta's mind",
+    review: "See what's on Atlanta's mind",
     link: "https://discord.gg/x3jKSusHk5",
   },
   {
     name: "#INTRO",
     job: "🆓",
     image: "https://em-content.zobj.net/source/apple/391/microphone_1f3a4.png",
-    review: `Grab the mic and let ATL5D know who you are, what you do, and your ATL5D Wallo address to receive $ATL tips.`,
+    review: "Grab the mic and let ATL5D know who you are, what you do, and your ATL5D Wallo address to receive $ATL tips.",
     link: "https://discord.gg/93YCshjGHb",
   },
   {
     name: "#MARKET",
     job: "🆓",
     image: "https://em-content.zobj.net/source/apple/391/handshake_1f91d.png",
-    review:
-      "Discover new ATL D-merch is for sale that can be resold for more than you purchased it for.",
+    review: "Discover new ATL D-merch is for sale that can be resold for more than you purchased it for.",
   },
   {
     name: "#NEIGHBORHOODS",
     job: "▶️",
     image: "https://em-content.zobj.net/source/apple/391/house_1f3e0.png",
-    review:
-      "Discover what's happening in real-time in every neighborhood in Atlanta in one channel.",
+    review: "Discover what's happening in real-time in every neighborhood in Atlanta in one channel.",
   },
   {
     name: "#ASK-ATL",
     job: "▶️",
     image: "https://em-content.zobj.net/source/apple/391/heart-exclamation_2763-fe0f.png",
-    review:
-      "Ask Atlanta in the #ask-atl channel.Create a poll with the command of /poll.",
+    review: "Ask Atlanta in the #ask-atl channel. Create a poll with the command of /poll.",
   },
   {
     name: "#ONLY-PETS",
     job: "▶️",
     image: "https://em-content.zobj.net/source/apple/391/smiling-cat-with-heart-eyes_1f63b.png",
-    review:
-      "Buy,sell or discuss pets in the #only-pets channel.",
+    review: "Buy, sell or discuss pets in the #only-pets channel.",
   },
   {
     name: "#CABCAST",
     job: "▶️",
     image: "https://em-content.zobj.net/source/apple/391/vertical-traffic-light_1f6a6.png",
-    review:
-      "Cabcast is the new Uber with no middleman, drivers earn 💯%. Challenge a driver with a live delivery event episode.",
+    review: "Cabcast is the new Uber with no middleman, drivers earn 💯%. Challenge a driver with a live delivery event episode.",
   },
   {
     name: "#GM",
     job: "▶️",
     image: "https://em-content.zobj.net/source/apple/391/sun-with-face_1f31e.png",
-    review:
-      "Start your day off with positive vibes in the GM channel.",
+    review: "Start your day off with positive vibes in the GM channel.",
   },
   {
     name: "#RNTALS",
     job: "▶️",
     image: "https://em-content.zobj.net/source/apple/391/houses_1f3d8-fe0f.png",
-    review:
-      "RNTALS is the new Airbnb. Accept $RNT for your properties with no middleman fees. ",
+    review: "RNTALS is the new Airbnb. Accept $RNT for your properties with no middleman fees.",
   },
   {
     name: "#HAIR",
     job: "▶️",
     image: "https://em-content.zobj.net/source/apple/391/large-red-circle_1f534.png",
-    review:
-      "Atlanta hair stylist sells or teaches hair, bookings, and accepts Bitcoin HAI$ money with middleman fees.",
+    review: "Atlanta hair stylist sells or teaches hair, bookings, and accepts Bitcoin HAI$ money with middleman fees.",
   },
   {
     name: "#FOODIE",
     job: "▶️",
     image: "https://em-content.zobj.net/source/apple/391/hamburger_1f354.png",
-    review:
-      "Promote the local Atlanta food industry to earn $ATL rewards, food and money.",
+    review: "Promote the local Atlanta food industry to earn $ATL rewards, food and money.",
   },
   {
     name: "#MUSIC",
     job: "▶️",
     image: "https://em-content.zobj.net/source/apple/391/musical-note_1f3b5.png",
-    review:
-      "ATL5D is the music industry. Head over to ATL5D.shop to tokenize your music to earn 💯%.",
+    review: "ATL5D is the music industry. Head over to ATL5D.shop to tokenize your music to earn 💯%.",
   },
   {
     name: "#SPIRITUAL",
     job: "▶️",
     image: "https://em-content.zobj.net/source/apple/391/sparkles_2728.png",
-    review:
-      "Discover your new ATL5D spiritual tribe.",
+    review: "Discover your new ATL5D spiritual tribe.",
   },
   {
     name: "#GAMERS",
     job: "▶️",
     image: "https://em-content.zobj.net/source/apple/391/video-game_1f3ae.png",
-    review:
-      "ATL5D gamers united in one #gamers channel, play ATL5D games, ATLverse games or casino games.",
+    review: "ATL5D gamers united in one #gamers channel, play ATL5D games, ATLverse games or casino games.",
   },
   {
     name: "#VOICE-CHAT",
     job: "▶️",
     image: "https://em-content.zobj.net/source/apple/391/couch-and-lamp_1f6cb-fe0f.png",
-    review:
-      "Listen to live ATL5D entertainment from your couch.",
+    review: "Listen to live ATL5D entertainment from your couch.",
   },
   {
     name: "#DJ-BOOTH",
     job: "▶️",
     image: "https://em-content.zobj.net/source/apple/391/headphone_1f3a7.png",
-    review:
-      "Listen to your favorite Atlanta DJ from anywhere in the world. Show them love by making it rain $ATL on em.",
+    review: "Listen to your favorite Atlanta DJ from anywhere in the world. Show them love by making it rain $ATL on them.",
   },
   {
     name: "#COOKING",
     job: "▶️",
     image: "https://em-content.zobj.net/source/apple/391/woman-cook-medium-skin-tone_1f469-1f3fd-200d-1f373.png",
-    review:
-      "Find out who is cooking up a fresh ATL plate that you can buy in the ATL5D.shop to create affordable food options.",
+    review: "Find out who is cooking up a fresh ATL plate that you can buy in the ATL5D.shop to create affordable food options.",
   },
   {
     name: "#CAREER",
     job: "▶️",
     image: "https://em-content.zobj.net/source/apple/391/briefcase_1f4bc.png",
-    review:
-      "See who's hiring in ATL.",
+    review: "See who's hiring in ATL.",
   },
   {
     name: "#MOVIE",
     job: "▶️",
     image: "https://em-content.zobj.net/source/apple/391/movie-camera_1f3a5.png",
-    review:
-      "ATL5D is the new Hollywood. Buy and sell movies, short films, or commercial ads that we promote on our socials.",
+    review: "ATL5D is the new Hollywood. Buy and sell movies, short films, or commercial ads that we promote on our socials.",
   },
   {
     name: "#PICS",
     job: "▶️",
     image: "https://em-content.zobj.net/source/apple/391/camera-with-flash_1f4f8.png",
-    review:
-      "Claim your FREE ATLnight🔴 pic that is airdropped to your Wallo so you can sell it or pass it down your kids as a digital asset.",
+    review: "Claim your FREE ATLnight🔴 pic that is airdropped to your Wallo so you can sell it or pass it down your kids as a digital asset.",
   },
   {
     name: "#BEAUTY",
     job: "▶️",
     image: "https://em-content.zobj.net/source/apple/391/nail-polish_1f485.png",
-    review:
-      "Join the Beauty channel to find local beauticians ATL5D.shop merch.",
+    review: "Join the Beauty channel to find local beauticians ATL5D.shop merch.",
   },
 ];
